760927f610489f5d5134f5edbdfeadb6c27ed9f9
Qria <qria@qria.net>
2018-11-13 19:09:56 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 from setuptools import setup
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(name='pyeon',
-      version='0.0.1',
-      description='''Generates some phrases when someone goes to the\
+      version='0.0.2',
+      description='''Generates some phrases when someone goes to the \
 convenience store.''',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='http://github.com/qria/pyeon',
       author='MinJune Kim',
       author_email='qria' '@' 'qria.net',
       license='MIT',
       packages=['pyeon'],
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ],
       zip_safe=False)
